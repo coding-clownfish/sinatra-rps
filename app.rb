ab52ffcf445427aca1c8e6f22a2cd98bdc5cf92f
coding-clownfish <204938594+coding-clownfish@users.noreply.github.com>
2025-04-13 22:56:59 +0000

    in progress

--- app.rb
+++ app.rb
@@ -1,9 +1,75 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
-get("/") do
+get("/homepage") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <p></p>
+
+
+  <h1>Welcome to Rock-Paper-Scissors!</h1>
+  <p>Rock-paper-scissors (also known as paper, scissors, stone or other variants) is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand.</p>
   "
+  erb(:elephant)
 end
+
+get("/rock") do
+  
+  erb(:elephant)
+
+counter=["rock","paper","scissors"].sample
+
+if counter=="rock"
+  result="We tied"
+elsif counter=="paper"
+  result="We lose"
+else counter=="scissors"
+  result="We win"
+end
+
+outcome = "We played rock! They played #{counter}! #{result}!"
+
+"<p>#{outcome}</p>"
+end
+
+
+get("/paper") do
+  
+  erb(:elephant)
+
+  counter=["rock","paper","scissors"].sample
+  
+  if counter=="paper"
+    result="We tied"
+  elsif counter=="scissors"
+    result="We lose"
+  else counter=="rock"
+    result="We win"
+  end
+  
+  outcome = "We played paper! They played #{counter}! #{result}!"
+  
+  "<p>#{outcome}</p>"
+  end
+
+  get("/scissors") do
+    
+    erb(:elephant)
+    
+    counter=["rock","paper","scissors"].sample
+    
+    if counter=="scissors"
+      result="We tied"
+    elsif counter=="rock"
+      result="We lose"
+    else counter=="paper"
+      result="We win"
+    end
+    
+    outcome = "We played scissors! They played #{counter}! #{result}!"
+    
+    "<p>#{outcome}</p>"
+    end
+    
+  
